f67ebdfaf4a0cb93663575e780536c69792fb697
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2022-03-25 13:09:54 -0400

    well I got the grid of squares but it aint centered

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css'
 
+const Square = (props) => {
+  // const classes = `square-color-${color}`
+  return <div className="square"></div>
+}
+
 const Board = (props) => {
+  const grid = []
+  for (let row = 0; row < 20; row++) {
+    grid.push([])
+    for (let col = 0; col < 20; col++) {
+      grid[row].push(<Square key={`${col}${row}`} />)
+    }
+  }
   return (
     <div className="container">
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
+      {grid}
     </div>
   )
 }
