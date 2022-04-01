93478779410f88b356015db68d82d9101d0284bb
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2022-04-01 15:31:41 -0400

    cba to work on this anymore

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ const rows = 20
 const cols = 20
 
 const Header = ({ length }) => {
-  // if (state === "loss") {
-  //     return (
-  //       <div className='header-container'>
-  //         <div className='game-over'>You lost. Click here to restart.</div>
-  //         <button onClick={restart}>Try again</button>
-  //       </div>
-  //     )
-  //   }
 
     return (
       <div className="header-container">
@@ -25,7 +17,7 @@ const Header = ({ length }) => {
 const LossScreen = (props) => {
   return (
     <div>
-      <h1>You lost. Your score was: {props.score}</h1>
+      <h1>You lost. Your score was {props.score}</h1>
       <h3>Try again?</h3>
       <button onClick={props.handleRestart}>Restart</button>
     </div>
@@ -35,31 +27,20 @@ const LossScreen = (props) => {
 function App() {
   const createBoard = () => [...Array(cols)].map((_) => [...Array(rows)].map((_) => "empty"))
 
-  const [snakePosition, setSnakePosition] = useState([[5, 5], [5, 6],])
+  const [snakePosition, setSnakePosition] = useState([[5, 5],])
   const [food, setFoodPosition] = useState([getRandomArr()])
   const [foodEaten, setFoodEaten] = useState(false)
-  const [direction, setDirection] = useState('DOWN')
+  const [direction, setDirection] = useState('RIGHT')
   const [grid, setGrid] = useState(createBoard())
   const [lossState, setLossState] = useState(false)
+  const [speed, setSpeed] = useState(220)
   // const [snakeHead, setSnakeHead] = useState([snakePosition[snakePosition.length - 1]])
   // maybe add state for head if code gets too wet
-  
-  function getRandomArr() {
-    let min = 0;
-    let max = 19;
-    max = Math.floor(max);
-    min = Math.ceil(min);
-     //The maximum is exclusive and the minimum is inclusive
-    let x = Math.floor(Math.random() * (max - min) + min)
-    let y = Math.floor(Math.random() * (max - min) + min)
-    return [x, y];
-  }
-
   useEffect(() => {
     handleFoodEating()
+    checkCollision()
     changeGridTypes(grid)
-    setTimeout(() => moveSnake(snakePosition), 150)
-    console.log(snakePosition)
+    setTimeout(() => moveSnake(snakePosition), 200)
   }, [snakePosition])
 
   useEffect(() => {
@@ -91,11 +72,25 @@ function App() {
     }
   }, [direction, setDirection]);
 
+  function getRandomArr() {
+    let min = 0;
+    let max = 20;
+    max = Math.floor(max);
+    min = Math.ceil(min);
+     //The maximum is exclusive and the minimum is inclusive
+    let x = Math.floor(Math.random() * (max - min) + min)
+    let y = Math.floor(Math.random() * (max - min) + min)
+    return [x, y];
+  }
+
+
+
+  
+
   const moveSnake = useCallback( 
     (snakePosition) => {
       let snake = snakePosition
       let newHead = snake[snake.length - 1]
-      let [x, y] = newHead
 
       switch (direction) {
         // left
@@ -121,18 +116,20 @@ function App() {
           newHead = [newHead[0] + 1, newHead[1]]
           break;
       }
-      
-      let eatenFood = foodEaten
 
-      if (checkCollision() || checkOutOfBounds(newHead)) {
-          setLossState(true)
-          return;
+      if (checkOutOfBounds(newHead)) {
+        setLossState(true)
+        return;
       } 
+    snake.push(newHead)
+    let eatenFood = foodEaten
+    eatenFood ? console.log('food eaten') : snake.shift()
+ 
 
-      snake.push(newHead)
-      eatenFood ? console.log('food eaten') : snake.shift()
-      setSnakePosition([...snake])  
-    }, [direction, grid, foodEaten])
+      
+    setSnakePosition([...snake])  
+    }, [direction, grid, foodEaten]
+  )
 
   const changeGridTypes = useCallback(
     (grid) => {
@@ -140,7 +137,7 @@ function App() {
       snakePosition.forEach(([x, y]) => newGrid[x][y] = "snake")
       food.forEach(([x, y]) => newGrid[x][y] = "food")
       setGrid(newGrid)
-    }, [snakePosition]
+    }, [snakePosition, food]
   )
 
   const checkOutOfBounds = (head) => {
@@ -151,15 +148,14 @@ function App() {
   const checkCollision = () => {
     let snake = [...snakePosition]
     let head = snake[snake.length - 1]
-    let [headX, headY] = head
     snake.pop()
-    snake.forEach((pos) => {
-      if (headX === pos[0] && headY === pos[1]) {
-        return true;
-      } else {
-        return false;
+
+    for (const pos of snake) {
+      if (head[0] === pos[0] && head[1] === pos[1]) {
+        setLossState(true)
+        return;
       }
-    })
+    }
   }
 
   const handleFoodEating = () => {
